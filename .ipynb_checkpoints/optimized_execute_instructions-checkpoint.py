486f5331c192c8c23ec38322fa27495243428803
Koyena Pal <pal.k@northeastern.edu>
2025-09-30 18:42:02 +0000

    current state

--- .ipynb_checkpoints/optimized_execute_instructions-checkpoint.py
+++ .ipynb_checkpoints/optimized_execute_instructions-checkpoint.py
@@ -15,11 +15,13 @@ random.seed(42)
 np.random.seed(42)
 os.environ['PYTHONHASHSEED'] = '42'
 
-INDUCTION_TASKS = ['cause_and_effect', 'larger_animal', 'num_to_verbal','orthography_starts_with',
-                   'rhymes', 'synonyms', 'taxonomy_animal', 'translation_en-fr',
-                   'reverse_from_middle', 'smallest_item_length', 'smallest_even_no_sqrt', 'most_vowel_return_consonant',
-                   'detect_rhyme_and_rewrite', 'rank_by_protein','multi_lang_to_english','square_of_zodiac_animal',
-                   'alternate_synonym_antonym', 'most_consonant_return_vowel', 'least_unique_word_count', 'first_word_alphabetically_return_reverse']
+# INDUCTION_TASKS = ['cause_and_effect', 'larger_animal', 'num_to_verbal','orthography_starts_with',
+#                    'rhymes', 'synonyms', 'taxonomy_animal', 'translation_en-fr',
+#                    'reverse_from_middle', 'smallest_item_length', 'smallest_even_no_sqrt', 'most_vowel_return_consonant',
+#                    'detect_rhyme_and_rewrite', 'rank_by_protein','multi_lang_to_english','square_of_zodiac_animal',
+#                    'alternate_synonym_antonym', 'most_consonant_return_vowel', 'least_unique_word_count', 'first_word_alphabetically_return_reverse']
+
+INDUCTION_TASKS = ['first_word_alphabetically_return_reverse']
 
 end_think_patterns = [
     r'</think>',
--- scripts/.ipynb_checkpoints/run_evaluate-checkpoint.sh
+++ scripts/.ipynb_checkpoints/run_evaluate-checkpoint.sh
@@ -5,21 +5,13 @@
 echo "Running Nemotron Ones..."
 # python execute_instructions.py --execution_engine nvidia/Nemotron-Research-Reasoning-Qwen-1.5B --input_dir data/induction_input --thought_type empty
 
-python evaluate.py --gen_model nvidia/Nemotron-Research-Reasoning-Qwen-1.5B --execution_input_dir data/induction_input --predictions_dir predictions_nvidia_Nemotron-Research-Reasoning-Qwen-1.5B_with_sampling
+python evaluate.py --gen_model nvidia/Nemotron-Research-Reasoning-Qwen-1.5B --execution_input_dir data/induction_input --predictions_dir predictions_nvidia_Nemotron-Research-Reasoning-Qwen-1.5B_with_sampling_without_answer
 
 # Command 2
 echo "Running GPT..."
 #python execute_instructions.py --execution_engine openai/gpt-oss-20b --input_dir data/induction_input --thought_type empty
 
-python evaluate.py --gen_model openai/gpt-oss-20b --execution_input_dir data/induction_input --predictions_dir predictions_openai_gpt-oss-20b_with_sampling
-
-
-# Command 3
-echo "Running openthinker..."
-
-#python execute_instructions.py --execution_engine open-thoughts/OpenThinker-7B --input_dir data/induction_input --thought_type empty
-
-python evaluate.py --gen_model open-thoughts/OpenThinker-7B --execution_input_dir data/induction_input --predictions_dir predictions_open-thoughts_OpenThinker-7B_with_sampling
+python evaluate.py --gen_model openai/gpt-oss-20b --execution_input_dir data/induction_input --predictions_dir predictions_openai_gpt-oss-20b_with_sampling_without_answer
 
 
 # Command 4
@@ -27,13 +19,24 @@ echo "Running Qwen/QwQ-32B..."
 
 #python execute_instructions.py --execution_engine Qwen/QwQ-32B --input_dir data/induction_input --thought_type empty
 
-python evaluate.py --gen_model Qwen/QwQ-32B --execution_input_dir data/induction_input --predictions_dir predictions_Qwen_QwQ-32B_with_sampling
+python evaluate.py --gen_model Qwen/QwQ-32B --execution_input_dir data/induction_input --predictions_dir predictions_Qwen_QwQ-32B_with_sampling_without_answer
 
 # Command 5
 echo "Running BytedTsinghua-SIA/DAPO-Qwen-32B..."
 
 #python execute_instructions.py --execution_engine BytedTsinghua-SIA/DAPO-Qwen-32B --input_dir data/induction_input --thought_type empty
 
-python evaluate.py --gen_model BytedTsinghua-SIA/DAPO-Qwen-32B --execution_input_dir data/induction_input --predictions_dir predictions_BytedTsinghua-SIA_DAPO-Qwen-32B_with_sampling
+python evaluate.py --gen_model BytedTsinghua-SIA/DAPO-Qwen-32B --execution_input_dir data/induction_input --predictions_dir predictions_BytedTsinghua-SIA_DAPO-Qwen-32B_with_sampling_without_answer
+
+# Command 3
+echo "Running openthinker..."
+
+#python execute_instructions.py --execution_engine open-thoughts/OpenThinker-7B --input_dir data/induction_input --thought_type empty
+
+python evaluate.py --gen_model open-thoughts/OpenThinker-7B --execution_input_dir data/induction_input --predictions_dir predictions_open-thoughts_OpenThinker-7B_with_sampling_without_answer
+
+
+python evaluate.py --gen_model open-thoughts/OpenThinker-7B --execution_input_dir data/induction_input --predictions_dir predictions_open-thoughts_OpenThinker-7B_with_sampling
+
 
 echo "All commands executed."
